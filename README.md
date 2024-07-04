440ba0b11075b204ce732d1d7939bf4bdf5ae583
Kailash <kailash61203@gmail.com>
2024-07-04 16:27:31 +0530

    ready to deploy

---
+++ README.md
@@ -0,0 +1,24 @@
+# Skineyra Waitlist Page
+
+## Tech Stack
+
+- Developed with **Next.js**.
+- Prefered **MailerLite** for email subscription.
+- Used **Tailwind** to style project.
+- Deployed on **Vercel**.
+
+## For Development
+
+Install packages.
+
+```bash
+  yarn
+```
+
+- Runs the app in the development mode.
+
+```bash
+  yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
---
+++ config/general.ts
@@ -0,0 +1,131 @@
+const config = {
+  domain: "theproduct.com",
+  title: "Skineyra Official",
+  description: "KKM APPROVED and DERMATOLOGICALLY TESTED",
+  subscribeForm:
+    "https://dashboard.mailerlite.com/forms/631411/101265918935434456/share",
+
+  socials: {
+    insta: "https://www.instagram.com/_mohammedsuhail3427/",
+  },
+
+  contents: {
+    title: "Discover Your Ideal Experience with Skineyra",
+    markOfTitle: "Skineyra", // its for marking the title. The text must contain on title. If not, it will be ignored.
+    description: `Our meticulously crafted skincare products are designed to nourish and enhance your natural beauty, ensuring you glow from within.`,
+
+    about: [
+      {
+        id: 1,
+        emoji: "💡",
+        title: "KKM Approved",
+        description:
+          "Rest assured, our products meet stringent safety standards set by the Ministry of Health Malaysia.",
+      },
+      {
+        id: 2,
+        emoji: "🚀",
+        title: "Dermatologically Tested",
+        description:
+          "Gentle on your skin, our formulations are rigorously tested to ensure effectiveness and compatibility.",
+      },
+      {
+        id: 3,
+        emoji: "🌎",
+        title: "Global Reach",
+        description:
+          "The Product is available to users all around the world. No matter where you are, you can access our platform and discover the perfect product for you.",
+      },
+    ],
+
+    howWorks: [
+      {
+        title: "Explore Our Products",
+        description:
+          "Browse our range of meticulously crafted skincare products designed to enhance your natural beauty.",
+      },
+      {
+        title: "Join the Waitlist",
+        description:
+          "Sign up to be the first to know when our products launch. Secure your spot and ensure you don't miss out on exclusive offers and early access.",
+      },
+      {
+        title: "Personalize suggestions",
+        description:
+          "Customize your emoji suggestions based on your preferences and writing style.",
+      },
+      {
+        title: "Stay Informed",
+        description:
+          "Receive updates directly in your inbox. We'll keep you informed about product launches, skincare tips, and special promotions.",
+      },
+      {
+        title: "Follow Us",
+        description:
+          "Stay inspired and informed. Follow Skineyra Official on social media for the latest updates, skincare advice, and more.",
+      },
+    ],
+
+    price: {
+      description:
+        "Unlock endless emoji possibilities with our monthly subscription. Cancel anytime, stress-free!",
+      price: "9$",
+
+      advantages: [
+        "Cancel anytime.",
+        "Analyze text emotions for suitable emoji suggestions.",
+        "Unlimited emoji access: Explore vast library.",
+        "Bullet points yield perfect results.",
+        "Ease of use.",
+      ],
+    },
+
+    faq: [
+      {
+        question: "What is Skineyra Official?",
+        answer:
+          "Skineyra Official is an exclusive skincare products platform designed to introduce you to our meticulously crafted skincare essentials, ensuring your journey towards radiant skin starts on the right foot.",
+      },
+      {
+        question: "How do I join Skineyra Official?",
+        answer: `Joining Skineyra Official is simple:
+
+          Visit our website.
+          Navigate to the waitlist section or homepage.
+          Enter your email address and click "Join Now" to secure your spot.`,
+      },
+
+      {
+        question: "How do I stay updated with Skineyra Official?",
+        answer:
+          "You'll receive email updates about product launches, skincare tips, and special promotions. Follow us on social media for additional updates and exclusive content.",
+      },
+      {
+        question: "Is Skineyra Official free to join?",
+        answer:
+          "Yes, joining Skineyra Official is completely free. Sign up now to access early bird offers and exclusive benefits.",
+      },
+      //   {
+      //     question: "Can I customize my emoji suggestions?",
+      //     answer:
+      //       "Yes! You can personalize your emoji suggestions based on your preferences and writing style.",
+      //   },
+      //   {
+      //     question: "Is The Product available globally?",
+      //     answer: "Yes! The Product is available to users all around the world.",
+      //   },
+      //   {
+      //     question: "How do I cancel my subscription?",
+      //     answer:
+      //       "You can cancel your subscription anytime. Simply go to your account settings and click 'cancel subscription'.",
+      //   },
+      {
+        question: "How do I contact Skineyra Official support?",
+        answer:
+          "For any inquiries or assistance: Visit our website and navigate to the contact section. Connect with us on social media for real-time support and updates.",
+      },
+    ],
+  },
+};
+
+export default config;
